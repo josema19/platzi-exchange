ac217b01ace2896ed99ec67d7e413621a458b7f0
José Manuel Barrientos Linares <josemacloud@gmail.com>
2021-05-28 10:34:50 -0500

    Project kick off

---
+++ src/api.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const url = 'https://api.coincap.io/v2'
+
+function getAssets() {
+    return axios.get(`${url}/assets?limit=20`)
+        .then(res => res.data.data)
+}
+
+function getAsset(coin) {
+    return axios.get(`${url}/assets/${coin}`)
+        .then(res => res.data.data)
+}
+
+function getAssetHistory(coin) {
+    const now = new Date();
+    const end = now.getTime();
+    now.setDate(now.getDate() - 1);
+    const start = now.getTime();
+    return axios.get(`${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`)
+        .then(res => res.data.data)
+}
+
+function getMarkets(coin) {
+    return axios.get(`${url}/assets/${coin}/markets?limit=5`)
+        .then(res => res.data.data)
+}
+
+function getExchange(id) {
+    return axios.get(`${url}/exchanges/${id}`)
+        .then(res => res.data.data)
+}
+
+export default {
+    getAssets,
+    getAsset,
+    getAssetHistory,
+    getMarkets,
+    getExchange,
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { VueSpinners } from '@saeris/vue-spinners';
+import '@/assets/css/tailwind.css';
+import Chartkick from 'vue-chartkick';
+import { Chart } from 'chart.js';
+import { dollarFilter, percentFilter } from '@/filters';
 
+import '@/router';
+import router from '@/router';
+
+Vue.use(VueSpinners)
+Vue.use(Chartkick.use(Chart))
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
